84c238f932fe6594b710f907b30af41a5c2fe881
michal <michal.lejwoda@gmail.com>
2025-03-11 22:22:32 +0100

    locals and virtual networks

--- main.tf
+++ main.tf
@@ -16,29 +16,64 @@ provider "azurerm" {
   subscription_id = "05f4e3ca-9790-4083-8531-f315f2fd6ebc"
 }
 
+locals {
+  resource_location="Poland Central"
+}
+
 resource "azurerm_resource_group" "appgrp" {
   name     = "app-grp"
-  location = "Poland Central"
+  location = local.resource_location
 }
 
-resource "azurerm_storage_account" "appstore09090894343" {
-  name                     = "appstore09090894343"
-  resource_group_name      = azurerm_resource_group.appgrp.name
-  location                 = azurerm_resource_group.appgrp.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
 
-}
-resource "azurerm_storage_container" "scripts" {
-  name                  = "scripts"
-  storage_account_name  = azurerm_storage_account.appstore09090894343.name
-
+resource "azurerm_network_security_group" "example" {
+  name                = "example-security-group"
+  location            = azurerm_resource_group.appgrp.location
+  resource_group_name = azurerm_resource_group.appgrp.name
 }
 
-resource "azurerm_storage_blob" "qaksk2s6vp8gszbc10202tjzt" {
-  name                   = "qaksk2s6vp8gszbc10202tjzt.png"
-  storage_account_name   = azurerm_storage_account.appstore09090894343.name
-  storage_container_name = azurerm_storage_container.scripts.name
-  type                   = "Block"
-  source                 = "qaksk2s6vp8gszbc10202tjzt.png"
+resource "azurerm_virtual_network" "app_network" {
+  name                = "app-network"
+  location            = azurerm_resource_group.appgrp.location
+  resource_group_name = azurerm_resource_group.appgrp.name
+  address_space       = ["10.0.0.0/16"]
+  # dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name             = "websubnet01"
+    address_prefixes = ["10.0.1.0/24"]
+  }
+
+  subnet {
+    name             = "appsubnet01"
+    address_prefixes = ["10.0.2.0/24"]
+    # security_group   = azurerm_network_security_group.example.id
+  }
+
+  # tags = {
+  #   environment = "Production"
+  # }
 }
+
+# resource "azurerm_storage_account" "appstore09090894343" {
+#   name                     = "appstore09090894343"
+#   resource_group_name      = azurerm_resource_group.appgrp.name
+#   location                 = azurerm_resource_group.appgrp.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+#
+# }
+# resource "azurerm_storage_container" "scripts" {
+#   name                  = "scripts"
+#   storage_account_name  = azurerm_storage_account.appstore09090894343.name
+#
+# }
+#
+# resource "azurerm_storage_blob" "qaksk2s6vp8gszbc10202tjzt" {
+#   name                   = "qaksk2s6vp8gszbc10202tjzt.png"
+#   storage_account_name   = azurerm_storage_account.appstore09090894343.name
+#   storage_container_name = azurerm_storage_container.scripts.name
+#   type                   = "Block"
+#   source                 = "qaksk2s6vp8gszbc10202tjzt.png"
+# }
+#
