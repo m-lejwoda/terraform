d0e12b446c02d124f3c04a5032a850a15a006f04
michal <michal.lejwoda@gmail.com>
2025-03-18 09:44:31 +0100

    assignemnt 4

--- main.tf
+++ main.tf
@@ -13,3 +13,18 @@ resource "azurerm_storage_account" "appstorageaccount123" {
   account_tier = "Standard"
   account_replication_type = "LRS"
 }
+
+resource "azurerm_storage_account" "appstorageaccount1235" {
+  name = "appstoreage4041233551"
+  resource_group_name = azurerm_resource_group.appgrp.name
+  location = azurerm_resource_group.appgrp.location
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "scripts" {
+  count = 3
+  name                  = "scripts${count.index}"
+  storage_account_id    = azurerm_storage_account.appstorageaccount1235.id
+  container_access_type = "private"
+}
