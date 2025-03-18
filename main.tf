ad38c8cf46e96908f6ed8a2d235816209bfb8ec9
michal <michal.lejwoda@gmail.com>
2025-03-18 10:26:14 +0100

    remove everything after for each

--- main.tf
+++ main.tf
@@ -5,33 +5,6 @@ resource "azurerm_resource_group" "appgrp" {
 
 
 
-resource "azurerm_storage_account" "appstorageaccount123" {
-  # count = 3
-  name = "appstoreage40412331"
-  resource_group_name = azurerm_resource_group.appgrp.name
-  location = azurerm_resource_group.appgrp.location
-  account_tier = "Standard"
-  account_replication_type = "LRS"
-}
-resource "azurerm_storage_container" "scripts" {
-  for_each = toset(["data", "scripts", "logs"])
-  name                 = each.key
-  storage_account_name = azurerm_storage_account.appstorageaccount123.name
-}
-
-resource "azurerm_storage_blob" "scripts" {
-  for_each = tomap({
-    script01 = "qaksk2s6vp8gszbc10202tjzt.png"
-    script02 = "qaksk2s6vp8gszbc10202tjzt1.png"
-    script03 = "qaksk2s6vp8gszbc10202tjzt2.png"
-  })
-  name                   = "${each.key}.png"
-  storage_account_name   = azurerm_storage_account.appstorageaccount123.name
-  storage_container_name = azurerm_storage_container.scripts["scripts"].name
-  type                   = "Block"
-  source = each.value
-}
-
 # output "container_name" {
 #   value = azurerm_storage_container.scripts["data"].name
 # }
