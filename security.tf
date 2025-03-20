04a039e5abbc1b6eeaa28b3bba4c743bff4001e8
michal <michal.lejwoda@gmail.com>
2025-03-20 10:08:24 +0100

    fix

--- security.tf
+++ security.tf
@@ -10,10 +10,10 @@
 resource "azurerm_key_vault_secret" "vmpassword" {
   name         = "vmpassword"
   value        = var.adminpassword
-  key_vault_id = data.azurerm_key_vault.appvault498943784353.id
+  key_vault_id = data.azurerm_key_vault.appvault49894378435344.id
 }
 
-data "azurerm_key_vault" "appvault498943784353"{
-  name = "appvault498943784353"
-  resource_group_name = azurerm_resource_group.appgrp.name
+data "azurerm_key_vault" "appvault49894378435344"{
+  name = "appvault49894378435344"
+  resource_group_name = "security-grp"
 }
