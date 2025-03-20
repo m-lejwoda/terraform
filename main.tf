73d5fb9625e0c3af495eacf88a09b8d5997947ba
michal <michal.lejwoda@gmail.com>
2025-03-20 11:17:40 +0100

    key vault

--- main.tf
+++ main.tf
@@ -55,6 +55,17 @@ resource "azurerm_network_security_group" "app_nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 310
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_subnet_network_security_group_association" "subnet_appnsg" {
