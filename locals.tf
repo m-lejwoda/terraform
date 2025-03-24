88902f1e5c0e9e783626aa0107d90ac31161a5f2
michal <michal.lejwoda@gmail.com>
2025-03-24 22:03:29 +0100

    tags done

--- locals.tf
+++ locals.tf
@@ -31,4 +31,8 @@
 
 locals {
   resource_location="Poland Central"
+  production_tags = {
+    production_code="${var.resource_tags.tags.department} ${var.resource_tags.tags.tier}-"
+    production_tier="${var.resource_tags.tags.tier}"
+  }
 }
