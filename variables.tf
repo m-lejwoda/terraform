f6ac927d3992920e92b73dd4bedf8592d64cd17a
michal <michal.lejwoda@gmail.com>
2025-03-30 14:00:35 +0200

    start mini project

--- variables.tf
+++ variables.tf
@@ -1,24 +1,19 @@
-variable "dbapp_environment" {
+variable "app_environment" {
   type = map(object({
-    server=map(object({
-      databases=map(object({
-        sku=string
-        sampledb=string
+    virtualnetworkname=string
+    virtualnetworkcidrblock=string
+    subnets=map(object({
+      cidrblock=string
+      machines=map(object({
+        networkinterfacename=string
+        publicipaddressname=string
       }))
     }))
   }))
 }
 
-variable "app_setup" {
-  type=list(string)
-}
-
-variable "webapp_environment" {
-  type = map(object({
-    serviceplan=map(object({
-      sku=string
-      os_type=string
-    }))
-    serviceapp=map(string)
-  }))
+variable "adminpassword" {
+  type=string
+  description = "This is the admin password for the virtual machine"
+  sensitive = true
 }
